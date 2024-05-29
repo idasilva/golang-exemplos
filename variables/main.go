2283cb0821484dee5140e260919de87363de1b0a
Isaias da Silva <isaias.silva@itera.com.br>
2024-05-29 20:32:59 -0300

    add variables exemplo

---
+++ variables/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+var (
+	one   string
+	two   string
+	three string
+)
+
+func main() {
+
+	one = "one"
+	two = "two"
+	three = "three"
+	four := "four"
+	var five = "five"
+	var six string
+	six = "six"
+
+	fmt.Println(one)
+	fmt.Println(two)
+	fmt.Println(three)
+	fmt.Println(four)
+	fmt.Println(five)
+	fmt.Println(six)
+}
